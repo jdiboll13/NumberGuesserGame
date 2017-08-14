fc24071c9c11372d7e691b634aeb44e977417ad6
Jeremiah Diboll <jdiboll@gmail.com>
2017-08-14 13:09:34 -0400

    Number Guesser Works in Console

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace RandomNumberGuesser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int number = randomNumber.Next(1,100);
+            Console.WriteLine("Pick a number from 1 to 100.");
+            var input = int.Parse(Console.ReadLine()); 
+            while (input != number)
+            {
+                if (input > number)
+                {
+                    Console.WriteLine("Your guess is too high.");
+                    input = int.Parse(Console.ReadLine());
+                } else if (input < number)
+                {
+                    Console.WriteLine("Your guess is too low.");
+                    input = int.Parse(Console.ReadLine());
+                }  
+            }
+            if (input == number)
+            {
+                Console.WriteLine("You guessed correctly");
+            }
+        }
+    }
+}
